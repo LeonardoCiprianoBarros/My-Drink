ed04ceb1bc9c9e3727f06883873211347247007b
LeonardoCiprianoBarros <leonardobarrosdev@gmail.com>
2023-02-15 21:35:11 -0300

    my drink

---
+++ faleconosco.js
@@ -0,0 +1,10 @@
+var dois =document.querySelector('.dois');
+
+dois.addEventListener('click', function(){
+    alert("Mensagem Cancelada");
+})
+
+var enviado=document.querySelector('.um');
+enviado.addEventListener('click', function(){
+    alert("Mensagem Enviada");})
+
